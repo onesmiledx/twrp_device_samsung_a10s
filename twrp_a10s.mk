51de81bad20a4b52c81b9fa4cfae885196fbf8ed
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-06-17 07:33:15 +0500

    Update twrp_a10s.mk

--- twrp_a10s.mk
+++ twrp_a10s.mk
@@ -15,16 +15,17 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Inherit from a10s device
+# Inherit from common vendor config
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/samsung/a10s/device.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a10s/recovery/root,recovery/root)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10s
