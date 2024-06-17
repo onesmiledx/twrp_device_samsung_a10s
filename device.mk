7de3af4d9f83300902d2a28cb59bbf2df6592605
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-06-17 07:42:18 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,19 +1,15 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 29
 
-LOCAL_PATH := device/samsung/a10s
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Screen
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1520
+DEVICE_RESOLUTION := 720x1520
