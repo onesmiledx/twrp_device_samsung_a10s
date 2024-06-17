b1ae5636e6ffca5bcbe4bbbcfe08173a07530029
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-06-17 21:46:32 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,12 +4,6 @@ PRODUCT_TARGET_VNDK_VERSION := 29
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+LOCAL_PATH := device/samsung/a10s
 
-# Screen
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1520
 DEVICE_RESOLUTION := 720x1520
