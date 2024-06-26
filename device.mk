f0473263dce280a84cefaf3641ea0321663e6013
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-06-26 12:29:53 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,3 +17,11 @@
 
 LOCAL_PATH := device/samsung/a10s
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
