3078e30f18b57c3a214ac6aeb85de4ee863efb66
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-07-14 09:17:14 +0500

    Update twrp_a10s.mk

--- twrp_a10s.mk
+++ twrp_a10s.mk
@@ -18,8 +18,8 @@
 PRODUCT_RELEASE_NAME := a10s
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from common vendor config
